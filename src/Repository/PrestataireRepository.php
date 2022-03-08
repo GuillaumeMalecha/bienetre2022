8c270aff3f1482a5eff04b199a160cfff968ac9a
Guillaume Malecha <guillaume.malecha@gmail.com>
2022-03-08 21:01:37 +0100

    update recherche

--- src/Repository/PrestataireRepository.php
+++ src/Repository/PrestataireRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\CategorieDeServices;
 use App\Entity\Prestataire;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -18,6 +19,69 @@ class PrestataireRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Prestataire::class);
     }
+
+    /**
+     * @return Prestataire[]
+     */
+    public function findAllWithJoins(){
+        $qb = $this->createQueryBuilder('p')
+            ->innerJoin('p.CategorieDeServices', 's')
+            ->leftJoin('p.localite', 'l')
+            ->addSelect('s');
+
+        $query = $qb->getQuery();
+        dd($query->getResult());
+        return $query->getResult();
+    }
+
+    public function findByCommune($commune){
+        $qb = $this->createQueryBuilder('p');
+        $qb->leftJoin('p.commune', 'communes')
+            ->where('communes = :commune')
+            ->setParameter(':commune', $commune);
+        //$qb->orderBy('p.commune');
+        return $qb->getQuery()->getResult();
+    }
+
+    public function findByService(CategorieDeServices $service)
+    {
+        $qb=$this->createQueryBuilder('p');
+
+        $qb->leftJoin('p.categorieServices', 'services');
+        $qb->where('services = :service')
+            ->setParameter(':service', $service);
+        return $qb->getQuery()->getResult();
+    }
+
+    public function findByPrestaCommuneService($nomPrestataire, $commune, $service){
+        $qb=$this->createQueryBuilder('p');
+        if($nomPrestataire!= null) {
+            $qb->where('p.nom = :nom')
+                ->setParameter(':nom', $nomPrestataire);
+        }
+        if($commune != null) {
+            $qb->innerJoin('p.commune', 'communes')
+                ->andWhere('communes.commune = :commune')
+                ->setParameter(':commune', $commune);
+        }
+        if($service != null) {
+            $qb->leftJoin('p.CategorieDeServices', 'services')
+                ->andWhere('services = :service')
+                ->setParameter(':service', $service);
+        }
+        return $qb->getQuery()->getResult();
+    }
+
+    public function lastsPrestataires()
+    {
+        $qb=$this->createQueryBuilder('p')
+            //->where('p.nom != null')
+            ->orderBy('p.id','DESC')
+            ->setFirstResult(0)
+            ->setMaxResults(6);
+        return $qb->getQuery()->getResult();
+    }
+
 
     // /**
     //  * @return Prestataire[] Returns an array of Prestataire objects
