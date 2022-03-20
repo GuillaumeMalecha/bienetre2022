fa2c30221ffda9ff62ed9731dbfc4eadd4cddd63
Guillaume Malecha <guillaume.malecha@gmail.com>
2022-03-20 19:09:57 +0100

    update

--- src/Form/InternauteType.php
+++ src/Form/InternauteType.php
@@ -5,6 +5,10 @@ namespace App\Form;
 use App\Entity\Internaute;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,6 +26,36 @@ class InternauteType extends AbstractType
             ->add('prenom', TextType::class, [
                 'label' => ' ',
                 'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Votre prénom']
+            ])
+            ->add('email', EmailType::class, [
+                'required'=>true,
+                'label' => ' ',
+                'attr'=>['class' => 'form-control b-r', 'placeholder'=>'Email']
+            ])
+            ->add('password', PasswordType::class, [
+                'invalid_message'=>'Les mots de passe doivent être identiques.',
+                'label' => ' ',
+                'attr'=>['class' => 'form-control b-r', 'placeholder'=>'Mot de passe']
+            ])
+            ->add('adresseNumero', NumberType::class, [
+                'label' => 'Adresse Postale',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Numéro']
+            ])
+            ->add('adresseRue', TextType::class, [
+                'label' => ' ',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Rue']
+            ])
+            ->add('codePostal', NumberType::class, [
+                'label' => ' ',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Code Postal']
+            ])
+            ->add('localite', TextType::class, [
+                'label' => ' ',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Localité']
+            ])
+            ->add('commune', TextType::class, [
+                'label' => ' ',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Commune']
             ])
             ->add('newsletter', ChoiceType::class, [
                 'choices' => [
--- src/Form/PrestataireType.php
+++ src/Form/PrestataireType.php
@@ -2,9 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\CategorieDeServices;
 use App\Entity\Prestataire;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -32,9 +37,44 @@ class PrestataireType extends AbstractType
                 'label' => ' ',
                 'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Numéro de TVA']
             ])
-            //->add('Internaute')
-            //->add('Utilisateur')
-            //->add('CategorieDeServices')
+            ->add('email', EmailType::class, [
+                'required'=>true,
+                'label' => ' ',
+                'attr'=>['class' => 'form-control b-r', 'placeholder'=>'Email']
+            ])
+            ->add('password', PasswordType::class, [
+                'invalid_message'=>'Les mots de passe doivent être identiques.',
+                'label' => ' ',
+                'attr'=>['class' => 'form-control b-r', 'placeholder'=>'Mot de passe']
+            ])
+            ->add('adresseNumero', NumberType::class, [
+                'label' => 'Adresse Postale',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Numéro']
+            ])
+            ->add('adresseRue', TextType::class, [
+                'label' => ' ',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Rue']
+            ])
+            ->add('codePostal', NumberType::class, [
+                'label' => ' ',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Code Postal']
+            ])
+            ->add('localite', TextType::class, [
+                'label' => ' ',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Localité']
+            ])
+            ->add('commune', TextType::class, [
+                'label' => ' ',
+                'attr' => ['class' => 'form-control b-r', 'placeholder' => 'Commune']
+            ])
+            ->add('categorieDeServices', EntityType::class,[
+                'class'=>CategorieDeServices::class,
+                'multiple' => true,
+                'expanded' => true,
+                'required' => false,
+            ])
+
+
             ->add('save', SubmitType::class, [
                 'label' => 'Créer mon compte professionnel',
                 'attr' => ['class' => 'btn btn-theme full-width']
